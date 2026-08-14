d26963cef27db60bbe2d66a427f60e3b5f7879a4
tatsiana <tatsiana.khazhevets@gmail.com>
2026-08-15 00:17:27 +0200

    Homework Parallel Tests

--- src/test/java/apiTests/iteration2_senior/BaseTest.java
+++ src/test/java/apiTests/iteration2_senior/BaseTest.java
@@ -1,18 +1,10 @@
 package apiTests.iteration2_senior;
 
-import apiTests.iteration2_senior.models.AdminUsersResponse;
-import apiTests.iteration2_senior.skelethon.endpoints.Endpoint;
-import apiTests.iteration2_senior.skelethon.requests.ValidatedCrudRequester;
-import apiTests.iteration2_senior.specs.RequestSpecs;
-import apiTests.iteration2_senior.specs.ResponseSpecs;
-import apiTests.iteration2_senior.steps.AdminStep;
 import common.extensions.TimingExtension;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.util.List;
 
 @ExtendWith(TimingExtension.class)
 public class BaseTest {
@@ -26,19 +18,5 @@ public class BaseTest {
     @AfterEach
     public void afterTest() {
         softly.assertAll();
-    }
-
-    @AfterEach
-    public void cleanUp() {
-        softly.assertAll();
-        AdminStep.deleteUsers();
-
-        List<AdminUsersResponse> users = new ValidatedCrudRequester<AdminUsersResponse>(
-                RequestSpecs.adminSpec(),
-                Endpoint.ADMIN_USERS_GET,
-                ResponseSpecs.requestReturnsOk())
-                .getAll(AdminUsersResponse[].class);
-
-        softly.assertThat(users).isEmpty();
     }
 }
